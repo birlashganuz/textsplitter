a293e7a3ad7c85a5df52f1ba93b272912ead63a2
birlashganuz <birlashganuz@gmail.com>
2026-03-16 17:52:41 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function splitText() {
+  let text = document.getElementById("inputText").value;
+
+  let limit = parseInt(document.getElementById("wordLimit").value);
+
+  let words = text.trim().split(/\s+/);
+
+  let result = document.getElementById("result");
+
+  result.innerHTML = "";
+
+  for (let i = 0; i < words.length; i += limit) {
+    let chunk = words.slice(i, i + limit).join(" ");
+
+    let div = document.createElement("div");
+    div.className = "chunk";
+
+    let id = "part" + i;
+
+    div.innerHTML = `
+<h3>Part ${i / limit + 1}</h3>
+<p id="${id}">${chunk}</p>
+<button class="copyBtn" onclick="copyText('${id}')">Copy</button>
+`;
+
+    result.appendChild(div);
+  }
+}
+
+function copyText(id) {
+  let text = document.getElementById(id).innerText;
+
+  navigator.clipboard.writeText(text);
+
+  alert("Copied!");
+}
